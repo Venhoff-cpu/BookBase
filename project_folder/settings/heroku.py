2e0532ee3da359427ed153ac1d4efbfa60e7e0e9
Krzysztof Pacuszka <krzysztof.pacuszka@gmail.com>
2020-10-21 14:48:53 +0200

    troubleshooting

--- project_folder/settings/heroku.py
+++ project_folder/settings/heroku.py
@@ -14,6 +14,7 @@ env = environ.Env(
 # False if not in os.environ
 DEBUG = env('DEBUG')
 
+
 # Raises django's ImproperlyConfigured exception if SECRET_KEY not in os.environ
 SECRET_KEY = env('SECRET_KEY')
 
@@ -23,29 +24,3 @@ prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
 GOOGLE_BOOKS_API_KEY = env('GOOGLE_BOOKS_API_KEY')
-
-PROJECT_ROOT = BASE_DIR
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
-#  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-# REST framework settings
-REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': [
-        'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
-    ],
-    "DEFAULT_FILTER_BACKENDS": [
-        "django_filters.rest_framework.DjangoFilterBackend"
-    ],
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 50,
-    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.AllowAny",),
-}
